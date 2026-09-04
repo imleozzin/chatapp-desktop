65263f5d9fedd3786234fba03a461dbf0abb61ec
Leonardo Ramalho <155858887+imleozzin@users.noreply.github.com>
2026-09-04 16:53:35 -0300

    Delete preload.js

--- preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("electronAPI", {
-  getScreenSources: () => ipcRenderer.invoke("get-screen-sources"),
-  getConfig: () => ipcRenderer.invoke("get-config"),
-  setConfig: (partial) => ipcRenderer.invoke("set-config", partial),
-  onUpdateReady: (callback) => ipcRenderer.on("update-ready", callback),
-  installUpdate: () => ipcRenderer.send("install-update"),
-});
